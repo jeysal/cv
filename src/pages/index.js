354f86b4929b9dce4dccdae42cf4ccc60be2c836
Tim Seckinger <seckinger.tim@gmail.com>
2019-03-14 18:00:33 +0100

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import ProfilePic from '../components/ProfilePic';
-import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
