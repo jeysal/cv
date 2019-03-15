c1e17f0f3c8552b40f4a20ea7338b00f979e7eb2
Tim Seckinger <seckinger.tim@gmail.com>
2019-03-15 19:58:15 +0100

    improve history descriptions

--- src/data/history.js
+++ src/data/history.js
@@ -4,39 +4,64 @@ export const start = new Date('2014-01');
 export default [
   {
     from: '2019-01',
-    description: 'Jest core contributor',
+    description: (
+      <>
+        <a href="https://github.com/facebook/jest">Jest</a> core contributor
+      </>
+    ),
   },
   {
     from: '2018-10',
-    description: 'Software Engineer at adesso AG',
+    description: (
+      <>
+        Software Engineer at <a href="https://www.adesso.de/">adesso AG</a>
+      </>
+    ),
   },
   {
     from: '2017-07',
     to: '2018-09',
-    description: 'Part-time working student at adesso AG',
+    description: (
+      <>
+        Part-time working student at{' '}
+        <a href="https://www.adesso.de/">adesso AG</a>
+      </>
+    ),
   },
   {
     from: '2014-09',
     to: '2018-12',
-    description: [
-      'Student at Fachhochschule Dortmund University of Applies Sciences and Arts, concluded with Bachelor of Science in Software Engineering (original title "Softwaretechnik")',
-      <br />,
-      <small>Final grade 1.2 (scale 1.0 best to 4.0 worst)</small>,
-    ],
+    description: (
+      <>
+        Student at{' '}
+        <a href="https://www.fh-dortmund.de/">Fachhochschule Dortmund</a>{' '}
+        University of Applies Sciences and Arts, concluded with Bachelor of
+        Science in Software Engineering (original title "Softwaretechnik")
+        <br />
+        <small>Final grade 1.2 (scale 1.0 best to 4.0 worst)</small>
+      </>
+    ),
   },
   {
     from: '2014-08',
     to: '2017-06',
-    description: 'Vocational education at adesso AG, with parallel studies',
+    description: (
+      <>
+        Vocational education at <a href="https://www.adesso.de/">adesso AG</a>,
+        with parallel studies
+      </>
+    ),
   },
   {
     to: '2014-06',
-    description: [
-      'Secondary education, concluded with Abitur',
-      <br />,
-      <small>
-        Average grade 1.6 (equiv. A in US / UK, scale 1.0 best to 4.0 worst)
-      </small>,
-    ],
+    description: (
+      <>
+        Secondary education, concluded with Abitur
+        <br />
+        <small>
+          Average grade 1.6 (equiv. A in US / UK, scale 1.0 best to 4.0 worst)
+        </small>
+      </>
+    ),
   },
 ];
