7291384711b596ee6302163eff64e5b26b7c5f3b
Tim Seckinger <seckinger.tim@gmail.com>
2019-03-16 15:33:37 +0100

    fill out skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,8 +6,33 @@ const Skills = () => (
   <section>
     <h2>Skills</h2>
     <div className={infoStyles.infoGrid}>
-      <div>TODO</div>
-      <div>TODO</div>
+      <div>Paradigms</div>
+      <div>Functional, Functional & Reactive, Imperative, Object-oriented</div>
+      <div>Main interests</div>
+      <div>
+        Testing, Compilers & static analysis, Tooling & DX, Build systems, Web
+        technologies
+      </div>
+      <div>Testing</div>
+      <div>Integration, Unit, E2E, UI, TDD, Property-based</div>
+      <div>Languages</div>
+      <div>
+        <em>Primary:</em> JavaScript, TypeScript, HTML, CSS, Java
+        <br />
+        <em>Used</em>: Rust, ReasonML, Flow, Groovy, Golang, C++, C, Bash
+      </div>
+      <div>Tools</div>
+      <div>
+        <em>In-depth knowledge:</em> Jest, Babel, Gradle, Git, Vim, Linux
+        <br />
+        <em>Other:</em> Various CI/CD, macOS, Node.js & most of the JS ecosystem
+      </div>
+      <div>Libraries</div>
+      <div>
+        <em>Selection:</em> React, Angular, Cycle.js, RxJS, Spring Framework
+      </div>
+      <div>Databases</div>
+      <div>Neo4j, MongoDB, various SQL-based</div>
     </div>
   </section>
 );
