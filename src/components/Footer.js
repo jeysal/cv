13551f2d9262d95d221b0d09ba14074daf34d0ce
Tim Seckinger <seckinger.tim@gmail.com>
2019-03-22 14:28:35 +0100

    fix web version link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,21 +19,27 @@ const Footer = () => (
       site: {
         siteMetadata: { author, github },
       },
-    }) => (
-      <>
-        <hr />
-        <footer className={styles.footer}>
-          <div className={styles.footerWeb}>
-            Built by
-            {` `}
-            <a href={`https://github.com/${github}`}>{author}</a>
-          </div>
-          <div className={styles.footerPrint}>
-            Web version at <code>{`https://${github}.github.io/cv/`}</code>
-          </div>
-        </footer>
-      </>
-    )}
+    }) => {
+      const webVersionUrl = `https://${github}.github.io/cv`;
+      return (
+        <>
+          <hr />
+          <footer className={styles.footer}>
+            <div className={styles.footerWeb}>
+              Built by
+              {` `}
+              <a href={`https://github.com/${github}`}>{author}</a>
+            </div>
+            <div className={styles.footerPrint}>
+              Web version at{' '}
+              <a href={webVersionUrl}>
+                <code>{webVersionUrl}</code>
+              </a>
+            </div>
+          </footer>
+        </>
+      );
+    }}
   />
 );
 
