edb4803c2c652c8561c9facd7a80b30ec67f2937
Tim Seckinger <seckinger.tim@gmail.com>
2019-03-23 13:45:10 +0100

    phone number

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,56 +1,59 @@
 import * as React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
+import { useQueryParam, StringParam } from 'use-query-params';
 import age from 's-age';
 
 import styles from './Info.module.css';
 
-const Info = () => (
-  <StaticQuery
-    query={graphql`
-      query InfoQuery {
-        site {
-          siteMetadata {
-            author
-            dateOfBirth
-            email
-            github
-          }
+const Info = () => {
+  const {
+    site: {
+      siteMetadata: { author, dateOfBirth, email, github },
+    },
+  } = useStaticQuery(graphql`
+    query InfoQuery {
+      site {
+        siteMetadata {
+          author
+          dateOfBirth
+          email
+          github
         }
       }
-    `}
-    render={({
-      site: {
-        siteMetadata: { author, dateOfBirth, email, github },
-      },
-    }) => (
-      <section>
-        <h2>Personal & contact information</h2>
-        <div className={styles.infoGrid}>
-          <div>Name</div>
-          <div>{author}</div>
-          <div>Born</div>
-          <div>
-            {dateOfBirth} (age {age(dateOfBirth)})
-          </div>
-          <div>Nationality</div>
-          <div>German (EU citizenship)</div>
-          <div>Languages</div>
-          <div>English (fluent), German (native)</div>
-          <small>&nbsp;</small>
-          <small>&nbsp;</small>
-          <div>Email</div>
-          <div>
-            <a href={`mailto:${email}`}>{email}</a>
-          </div>
-          <div>GitHub</div>
-          <div>
-            <a href={`https://github.com/${github}`}>{github}</a>
-          </div>
+    }
+  `);
+  const [phone] = useQueryParam('phone', StringParam);
+
+  return (
+    <section>
+      <h2>Personal & contact information</h2>
+      <div className={styles.infoGrid}>
+        <div>Name</div>
+        <div>{author}</div>
+        <div>Born</div>
+        <div>
+          {dateOfBirth} (age {age(dateOfBirth)})
         </div>
-      </section>
-    )}
-  />
-);
+        <div>Nationality</div>
+        <div>German (EU citizenship)</div>
+        <div>Languages</div>
+        <div>English (fluent), German (native)</div>
+        <small>&nbsp;</small>
+        <small>&nbsp;</small>
+        <div>Email</div>
+        <div>
+          <a href={`mailto:${email}`}>{email}</a>
+        </div>
+        <div>Phone</div>
+        <div>{(phone && `+${phone}`) || '<hidden in public version>'}</div>
+        <div>GitHub</div>
+        <div>
+          <a href={`https://github.com/${github}`}>{github}</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Info;
